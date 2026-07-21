15c844f80e6b90591dac17b5208f9a56fbacc168
北 聖也 <spelling.import@gmail.com>
2026-07-21 09:59:22 +0900

    UserSeedを作った

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,29 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'username' => 'alice',
+                'email' => 'alice@example.com',
+                'password' => 'password',
+                'profile' => 'Laravelを学習中です。',
+            ],
+            [
+                'username' => 'bob',
+                'email' => 'bob@example.com',
+                'password' => 'password',
+                'profile' => 'Reactが好きです。',
+            ],
+            [
+                'username' => 'charlie',
+                'email' => 'charlie@example.com',
+                'password' => 'password',
+                'profile' => 'PHPとMySQLを勉強しています。',
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
